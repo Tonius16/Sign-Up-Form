f2d65534dba223d3cee04a2f771085c3977a5b4e
Toni <toni.cartman@gmail.com>
2023-05-08 17:13:17 +0200

    add more style to formand a some of JS

--- sign-up.js
+++ sign-up.js
@@ -1,9 +1,10 @@
-passInp = document.getElementById("password");
-passConfInp = document.getElementById("confPassword");
-passDiv = document.getElementById("passDiv");
-body = document.querySelector("body");
+const passInp = document.getElementById("password");
+const passConfInp = document.getElementById("confPassword");
+const passDiv = document.getElementById("passDiv");
+const confPassDiv = document.getElementById("confPassDiv");
+const body = document.querySelector("body");
 
-regex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+const regex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
 
 function checkPass() {
   if (passInp.value != passConfInp.value) {
@@ -11,8 +12,39 @@ function checkPass() {
   }
 }
 
-passInp.addEventListener("input", () => {
-  if (regex.test(passInp.value)) {
-    console.log("asd");
+/*passInp.addEventListener("click", () => {
+  if (regex.test(passInp.value) != true) {
+    regExText = document.createElement("p");
+    regExText.classList.add("rexExText");
+    regExText.textContent = "asd";
+    passDiv.appendChild(regExText);
   }
+});*/
+
+passConfInp.addEventListener("input", () => {
+  if (passInp.value === passConfInp.value) {
+    passConfInp.setCustomValidity("");
+  } else {
+    passConfInp.setCustomValidity("Please match password");
+  }
+});
+
+passInp.addEventListener("focus", () => {
+  enterPassTxt = document.createElement("p");
+  enterPassTxt.textContent = "Min 8 char, 1 letter and 1 number";
+  passDiv.appendChild(enterPassTxt);
+});
+
+passInp.addEventListener("blur", () => {
+  enterPassTxt.remove();
+});
+
+passConfInp.addEventListener("focus", () => {
+  enterPassConfTxt = document.createElement("p");
+  enterPassConfTxt.textContent = "Repeat password";
+  confPassDiv.appendChild(enterPassConfTxt);
+});
+
+passConfInp.addEventListener("blur", () => {
+  enterPassConfTxt.remove();
 });
