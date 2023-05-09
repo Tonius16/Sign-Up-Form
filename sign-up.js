1635376c1bfaa9a8fab8aefa910af9a5b85f59fc
Toni <toni.cartman@gmail.com>
2023-05-09 17:54:17 +0200

    add more style to formand a bit more JS

--- sign-up.js
+++ sign-up.js
@@ -3,23 +3,60 @@ const passConfInp = document.getElementById("confPassword");
 const passDiv = document.getElementById("passDiv");
 const confPassDiv = document.getElementById("confPassDiv");
 const body = document.querySelector("body");
+const passText = document.getElementById("passText");
+const confPassText = document.getElementById("confPassText");
+const nameInp = document.getElementById("name");
+const lastName = document.getElementById("lastName");
+const eMail = document.getElementById("e-mail");
+const telNum = document.getElementById("telNum");
+const nameSpan = document.getElementById("nameSpan");
+const lastNameSpan = document.getElementById("lastNameSpan");
+const eMailSpan = document.getElementById("e-mailSpan");
+const telNumSpan = document.getElementById("telNumSpan");
+const passSpan = document.getElementById("passSpan");
+const confPassSpan = document.getElementById("confPassSpan");
 
 const regex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
 
-function checkPass() {
-  if (passInp.value != passConfInp.value) {
-    console.log("jugo");
+nameInp.addEventListener("input", () => {
+  if (nameInp.checkValidity()) {
+    nameSpan.style.display = "block";
+  } else {
+    nameSpan.style.display = "none";
   }
-}
+});
 
-/*passInp.addEventListener("click", () => {
-  if (regex.test(passInp.value) != true) {
-    regExText = document.createElement("p");
-    regExText.classList.add("rexExText");
-    regExText.textContent = "asd";
-    passDiv.appendChild(regExText);
+lastName.addEventListener("input", () => {
+  if (lastName.checkValidity() && lastName.value != "") {
+    lastNameSpan.style.display = "block";
+  } else {
+    lastNameSpan.style.display = "none";
   }
-});*/
+});
+
+eMail.addEventListener("input", () => {
+  if (eMail.checkValidity()) {
+    eMailSpan.style.display = "block";
+  } else {
+    eMailSpan.style.display = "none";
+  }
+});
+
+telNum.addEventListener("input", () => {
+  if (telNum.checkValidity() && telNum.value != "") {
+    telNumSpan.style.display = "block";
+  } else {
+    telNumSpan.style.display = "none";
+  }
+});
+
+passInp.addEventListener("input", () => {
+  if (passInp.checkValidity()) {
+    passSpan.style.display = "block";
+  } else {
+    passSpan.style.display = "none";
+  }
+});
 
 passConfInp.addEventListener("input", () => {
   if (passInp.value === passConfInp.value) {
@@ -29,22 +66,29 @@ passConfInp.addEventListener("input", () => {
   }
 });
 
+passConfInp.addEventListener("input", () => {
+  if (passConfInp.checkValidity()) {
+    confPassSpan.style.display = "block";
+    console.log(passConfInp.checkValidity());
+  } else {
+    confPassSpan.style.display = "none";
+  }
+});
+
 passInp.addEventListener("focus", () => {
-  enterPassTxt = document.createElement("p");
-  enterPassTxt.textContent = "Min 8 char, 1 letter and 1 number";
-  passDiv.appendChild(enterPassTxt);
+  passText.textContent = "Min 8 char, 1 letter and 1 number";
+  passDiv.appendChild(passText);
 });
 
 passInp.addEventListener("blur", () => {
-  enterPassTxt.remove();
+  passText.textContent = "";
 });
 
 passConfInp.addEventListener("focus", () => {
-  enterPassConfTxt = document.createElement("p");
-  enterPassConfTxt.textContent = "Repeat password";
-  confPassDiv.appendChild(enterPassConfTxt);
+  confPassText.textContent = "Repeat password";
+  confPassDiv.appendChild(confPassText);
 });
 
 passConfInp.addEventListener("blur", () => {
-  enterPassConfTxt.remove();
+  confPassText.textContent = "";
 });
